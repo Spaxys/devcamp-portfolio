954b1c8d26edd4b20501f5b8006f9174be8603bb
Daniel Webioern <spaxys@live.se>
2019-10-18 12:04:36 +0200

    Built pages via controller generator

---
+++ app/controllers/test2_controller.rb
@@ -0,0 +1,10 @@
+class Test2Controller < ApplicationController
+  def foo
+  end
+
+  def bar
+  end
+
+  def zor
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'test2/foo'
+  get 'test2/bar'
+  get 'test2/zor'
+  get 'test/foo'
+  get 'pages/home'
+  get 'pages/about'
+  get 'pages/contact'
   resources :blogs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
