37119718b1c7290deeb28357eb99b89192cb7c62
Daniel Webioern <spaxys@live.se>
2019-11-04 16:53:47 +0100

    Fixed jquery dependency for rails 6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,18 @@ class ApplicationController < ActionController::Base
 	include SetSource
 	include DefaultPageContent
 	include CurrentUserConcern
+
+	before_action :set_copyright
+
+	def set_copyright
+		@copyright = DevcampViewTool::Renderer.copyright 'Phileas Fogg', 'All rights reserved'
+	end
+end
+
+module DevcampViewTool
+	class Renderer
+		def self.copyright name, msg
+			"&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+		end
+	end
 end
