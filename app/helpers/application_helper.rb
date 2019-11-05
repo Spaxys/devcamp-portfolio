7fe969cf79873edb9d6a3ae229eaba0ccfc40c97
Daniel Webioern <spaxys@live.se>
2019-11-05 15:55:07 +0100

    Updated master layout styles along with helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,11 @@
 module ApplicationHelper
-	def login_helper
+	def login_helper style
 		if current_user.is_a?(GuestUser)
-      (link_to "Register", new_user_registration_path) +
-      "<br>".html_safe +
-      (link_to "Login", new_user_session_path)
+      (link_to "Register", new_user_registration_path, class: style) +
+      " ".html_safe +
+      (link_to "Login", new_user_session_path, class: style)
     else
-      role_string = current_user.roles.each do |role| role.to_s end
-      role_display = "Current user role: #{role_string}"
-      content_tag(:p, role_display) + 
-      (link_to "Logout", destroy_user_session_path, method: :delete)
+      (link_to "Logout", destroy_user_session_path, method: :delete, class: style)
     end
   end
 
