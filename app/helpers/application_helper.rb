48fcbbd115ba9a8601df71a5fe573a8f0a6c4970
Daniel Webioern <spaxys@live.se>
2019-11-05 14:48:54 +0100

    Implemented authorization engine for portfolio items and blogs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,14 @@
 module ApplicationHelper
 	def login_helper
-		if current_user.is_a?(User)
-	    link_to "Logout", destroy_user_session_path, method: :delete
+		if current_user.is_a?(GuestUser)
+      (link_to "Register", new_user_registration_path) +
+      "<br>".html_safe +
+      (link_to "Login", new_user_session_path)
     else
-	    (link_to "Register", new_user_registration_path) +
-	    "<br>".html_safe +
-	    (link_to "Login", new_user_session_path)
+      role_string = current_user.roles.each do |role| role.to_s end
+      role_display = "Current user role: #{role_string}"
+      content_tag(:p, role_display) + 
+      (link_to "Logout", destroy_user_session_path, method: :delete)
     end
   end
 
